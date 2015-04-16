516f7c7edd073a6f21c4ca99a1604653f1e24a6a
hackeris <hackeris@qq.com>
2015-04-16 11:33:13 +0800

    correct django admin enable

--- eprint/urls.py
+++ eprint/urls.py
@@ -1,15 +1,15 @@
 from django.conf.urls import patterns, include, url
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
     # Examples:
     url(r'^$', 'eprint.views.home', name='home'),
     url(r'^register$','eprint.views.register', name='register'),
     url(r'^login$','eprint.views.user_login', name='user_login'),
-    url(r'^dashboard/',include('dashboard.urls'))
+    url(r'^dashboard/',include('dashboard.urls')),
     # url(r'^eprint/', include('eprint.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
