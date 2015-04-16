fcebcf051c8ffe710b52e0d7406d95b5b8ced8f0
hackeris <hackeris@qq.com>
2015-04-16 10:59:39 +0800

    add register page and views.py function register

--- eprint/views.py
+++ eprint/views.py
@@ -1,5 +1,11 @@
 from django.shortcuts import render_to_response
 from django.shortcuts import HttpResponse
+from django.shortcuts import RequestContext
 
 def home(request):
-	return render_to_response('index.html')
+	context = RequestContext(request)
+	return render_to_response('index.html',context)
+
+def register(request):
+	context = RequestContext(request)
+	return render_to_response('register.html',context)	
