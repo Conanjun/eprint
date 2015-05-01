995bd4e5f6939ce6aa6510de9d946d63802ac9c1
hackeris <hackeris@qq.com>
2015-05-01 13:56:38 +0800

    format code

--- dashboard/views.py
+++ dashboard/views.py
@@ -1,9 +1,9 @@
-#-*- coding=utf-8 -*-
+# -*- coding=utf-8 -*-
 # Create your views here.
-from django.shortcuts import render_to_response,render_to_response
+from django.shortcuts import render_to_response, render_to_response
 from django.shortcuts import HttpResponse
 from django.shortcuts import RequestContext
 
 
 def dashboard(request):
-	return render_to_response('dashboard.html')
+    return render_to_response('dashboard.html')
