5e698b1bb7d75361223b0d611eee3295fe490bd6
Conan <1526840124@qq.com>
2015-05-03 14:04:09 +0800

    add the admin login function

--- backend/urls.py
+++ backend/urls.py
@@ -2,5 +2,7 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-    url(r'login','backend.views.admin_login',name='admin_login'),
+   # url(r'^$''backend.views.admin_login',name='admin_login_default'),
+    url(r'^login','backend.views.admin_login',name='admin_login'),
+    url(r'^api/login','backend.views.api_admin_login',name='api_admin_login'),
 )
--- backend/views.py
+++ backend/views.py
@@ -4,8 +4,33 @@ from django.http import HttpResponse, HttpResponseRedirect
 from django.template import RequestContext
 from django import forms
 from django.contrib.auth.models import User
+from django.contrib.auth import login, authenticate
+from django.views.decorators.csrf import csrf_protect
+
+
+class AdminLoginForm(forms.Form):
+    email = forms.EmailField(label='email', widget=forms.widgets.TextInput())
+    password = forms.CharField(label='password', widget=forms.PasswordInput())
 
 
 def admin_login(request):
-     return render_to_response('admin_login.html')
-     #return HttpResponse('admin login ok')
+    context = RequestContext(request)
+    return render_to_response('admin_login.html',context)
+
+def api_admin_login(request):
+    if request.method == 'POST':
+        admin_form = AdminLoginForm(request.POST)
+        if admin_form.is_valid():
+            email = admin_form.cleaned_data['email']
+            password = admin_form.cleaned_data['password']
+            admin_user = User.objects.get(email=email)
+            admin_user_auth = authenticate(username = admin_user.username, password=password)
+            if admin_user_auth.is_staff is True:
+                login(request, admin_user_auth)
+                return HttpResponse('Login Ok')
+            else:
+                return HttpResponse('Login fail')
+    else:
+        admin_form = AdminLoginForm()
+    return HttpResponse('Login fail')
+    #return HttpResponseRedirect('/backend/login')
