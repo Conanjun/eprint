fd2e0e9c5ceef5fdbf2b3358948cff02ae796a87
hackeris <hackeris@qq.com>
2015-05-12 23:52:41 +0800

    removed the api/login from backend

--- backend/urls.py
+++ backend/urls.py
@@ -4,7 +4,6 @@ from django.conf.urls import patterns, include, url
 urlpatterns = patterns('',
                        url(r'^$','backend.views.admin_login', name='admin_login_default'),
                        url(r'^login', 'backend.views.admin_login', name='admin_login'),
-                       url(r'^api/login', 'backend.views.api_admin_login', name='api_admin_login'),
                        url(r'^index', 'backend.views.admin_index', name='admin_index'),
                        url(r'^print_orders_list', 'backend.views.admin_print_orders', name='admin_print_orders'),
                        url(r'^trial_orders_list', 'backend.views.admin_trial_orders', name='admin_trial_orders'),
