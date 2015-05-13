6a61963627f9745476b546b52f389f06577f773f
hackeris <hackeris@qq.com>
2015-05-13 23:39:54 +0800

    add support for user profile

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,7 +8,9 @@ from dashboard.models import UserProfile
 def dashboard(request):
     user = request.user
     if user.is_authenticated():
+        user_profile = UserProfile.objects.get(user=user)
         context = RequestContext(request)
+        context['user_profile'] = user_profile
         return render_to_response('dashboard.html', context)
     else:
         return HttpResponseRedirect('login')
@@ -23,12 +25,12 @@ def update_profile(request):
         building = request.GET['building']
         gender = request.GET['gender']
 
-        user_profile = UserProfile.get(user=user)
+        user_profile = UserProfile.objects.get(user=user)
         user_profile.phone_number = phone
         user_profile.building = building
         user_profile.gender = gender
         user_profile.number = student_number
         user_profile.save()
-        return HttpResponseRedirect('dashboard')
+        return HttpResponseRedirect('/dashboard')
     else:
         return HttpResponseRedirect('login')
