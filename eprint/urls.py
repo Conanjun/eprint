92bf6164d4838f50a3177097ed273b3623f9f3c5
hackeris <hackeris@qq.com>
2015-05-13 11:39:46 +0800

    removed unused url config

--- eprint/urls.py
+++ eprint/urls.py
@@ -6,29 +6,18 @@ from django.contrib import admin
 admin.autodiscover()
 
 urlpatterns = patterns('',
-                       # Examples:
                        url(r'^$', 'eprint.views.home', name='home'),
                        url(r'^register$', 'eprint.views.register', name='register'),
                        url(r'^api/register$', 'eprint.views.api_register', name='api_register'),
                        url(r'^login$', 'eprint.views.user_login', name='user_login'),
                        url(r'^contact$', 'eprint.views.contact', name='contact'),
-#                       url(r'^success$', 'eprint.views.show_success', name='show_success'),
 
                        url(r'^dashboard$', include('dashboard.urls')),
 
-                       url(r'^havetry$', 'order.views.have_try', name='have_try'),
-
                        url(r'^trial_order$', 'order.views.trial_order', name='trial_order'),
                        url(r'^print_order$', 'order.views.print_order', name='print_order'),
-                       #url(r'backend','backend.views.admin_login',name='admin_login'),
                        url(r'^backend/',include('backend.urls')),
 
-                       # url(r'^eprint/', include('eprint.foo.urls')),
-
-                       # Uncomment the admin/doc line below to enable admin documentation:
-                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
-
-                       # Uncomment the next line to enable the admin:
                        url(r'^admin/', include(admin.site.urls)),
 )
 
