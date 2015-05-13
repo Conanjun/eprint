6ea52e0c40a5db764d97cb53a16e75a364eedac9
hackeris <hackeris@qq.com>
2015-05-13 11:29:42 +0800

    fix dashboard indent bug

--- dashboard/views.py
+++ dashboard/views.py
@@ -8,26 +8,27 @@ from dashboard.models import UserProfile
 def dashboard(request):
     user = request.user
     if user.is_authenticated():
-    	context = RequestContext(request)
-	    return render_to_response('dashboard.html', context)
-	return HttpResponseRedirect('login')
+        context = RequestContext(request)
+        return render_to_response('dashboard.html', context)
+    else:
+        return HttpResponseRedirect('login')
 
 
 def update_profile(request):
-	user = request.user
-	if user.is_authenticated():
-		name = request.GET['name']
-		tel = request.GET['tel']
-		number = request.GET['number']
-		building = request.GET['building']
-		gender = request.GET['gender']
+    user = request.user
+    if user.is_authenticated():
+        name = request.GET['name']
+        phone = request.GET['phone']
+        student_number = request.GET['student_number']
+        building = request.GET['building']
+        gender = request.GET['gender']
 
-		user_profile = UserProfile.get(user=user)
-		user_profile.phone_number = tel
-		user_profile.building = building
-		user_profile.gender = gender
-		user_profile.number = number
-		user_profile.save()
-		return HttpResponseRedirect('dashboard')
-	else:
-		return HttpResponseRedirect('login')
+        user_profile = UserProfile.get(user=user)
+        user_profile.phone_number = phone
+        user_profile.building = building
+        user_profile.gender = gender
+        user_profile.number = student_number
+        user_profile.save()
+        return HttpResponseRedirect('dashboard')
+    else:
+        return HttpResponseRedirect('login')
