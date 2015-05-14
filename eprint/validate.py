f6d05bec59954b65e3ec05ff7a22e31a2ba57c41
hackeris <hackeris@qq.com>
2015-05-14 08:53:58 +0800

    refactored the name of validation functions

--- eprint/validate.py
+++ eprint/validate.py
@@ -2,41 +2,41 @@
 import re
 
 
-def validate_Name(name):
+def validate_name(name):
     if len(name) < 40:
         if re.match('^[a-zA-Z0-9_\u4e00-\u9fa5]+$', name):
             return True
     return False
 
 
-def validate_Email(email):
+def validate_email(email):
     if len(email) > 7:
         if re.match('\w+([-+.]\w+)*@\w+([-.]\w+)*\.\w+([-.]\w+)*', email) != None:
             return True
     return False
 
 
-def validate_Phone(phone):
+def validate_phone(phone):
     if len(phone) > 6:
         if re.match('1\d{10}', phone) != None:
             return True
     return False
 
 
-def validate_StudentNumber(studentnumber):
-    if len(studentnumber) > 6:
-        if re.match('^[0-9]+$') != None:
+def validate_student_number(student_number):
+    if len(student_number) > 6:
+        if re.match('^[0-9]+$') is not None:
             return True
     return False
 
 
 # add in the future
-def validate_Building(building):
+def validate_building(building):
     return True
 
 
-def validate_Gender(gender):
-    if re.match('^[0,1]{1}$', gender) != None:
+def validate_gender(gender):
+    if re.match('^[0,1]{1}$', gender) is not None:
         return True
     return False
 
@@ -50,27 +50,27 @@ def validate_file(filename):
 
 
 def validate_status(status):
-    if re.match('^[0,1]{1}$', status) != None:
+    if re.match('^[0,1]{1}$', status) is not None:
         return True
     return False
 
 
 def validate_color(color):
-    if re.match('^[0,1]{1}$', str(color)) != None:
+    if re.match('^[0,1]{1}$', str(color)) is not None:
         return True
     return False
 
 
 def validate_method(method):
-    if re.match('^[0,1]{1}$', str(method)) != None:
+    if re.match('^[0,1]{1}$', str(method)) is not None:
         return True
     return False
 
 
-update_profile_validate = {"name": validate_Name, "phone": validate_Phone, 'student_number': validate_StudentNumber,
-                           'building': validate_Building, 'gender': validate_Gender}
+update_profile_validate = {"name": validate_name, "phone": validate_phone, 'student_number': validate_student_number,
+                           'building': validate_building, 'gender': validate_gender}
 
-trial_order_validate = {"name": validate_Name, "phone": validate_Phone, 'building': validate_Building,
+trial_order_validate = {"name": validate_name, "phone": validate_phone, 'building': validate_building,
                         'status': validate_status, 'filetype': validate_file}
 
 print_order_validate = {'status': validate_status, 'filetype': validate_file, 'color': validate_color,
