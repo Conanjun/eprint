5d862c01381e32c4712366505d832966956172d3
hackeris <hackeris@qq.com>
2015-05-14 11:24:49 +0800

    add models for Building and BuildingGroup for UserProfile

--- dashboard/models.py
+++ dashboard/models.py
@@ -4,11 +4,24 @@ from django.contrib.auth.models import User
 # Create your models here.
 
 
+class BuildingGroup(models.Model):
+    name = models.CharField(max_length=20)
+
+
+class Building(models.Model):
+    group = models.ForeignKey(BuildingGroup)
+    name = models.CharField(max_length=20)
+
+
+def get_buildings():
+    return BuildingGroup.objects.all()
+
+
 class UserProfile(models.Model):
     user = models.OneToOneField(User)
     name = models.CharField(max_length=9)
     phone_number = models.CharField(max_length=20)
-    building = models.CharField(max_length=4)
+    building = models.ForeignKey(Building)
     gender = models.IntegerField()
     number = models.CharField(max_length=15)
 
