cb554497e855bc80e56069a67d79d16e9bdecce2
hackeris <hackeris@qq.com>
2015-05-14 09:10:15 +0800

    refactored the validate of orders in app order

--- order/views.py
+++ order/views.py
@@ -13,7 +13,6 @@ class PrintOrderForm(forms.Form):
     file = forms.FileField()
     color = forms.CharField()
     method = forms.CharField()
-    tel = forms.CharField()
 
 
 class TrialOrderForm(forms.Form):
@@ -27,63 +26,63 @@ def have_try(request):
     return HttpResponse('Have try')
 
 
+def validate_print_order_form(form):
+    color = form.cleaned_data['color']
+    method = form.cleaned_data['method']
+    up_file = form.cleaned_data['file']
+    if validate.print_order_validate['filetype'](up_file.name)\
+            and validate.print_order_validate['color'](str(color))\
+            and validate.print_order_validate['method'](str(method)):
+        return True
+    return False
+
+
 def print_order(request):
     if request.method == "POST":
-        print request.POST
-        order = PrintOrderForm(request.POST, request.FILES)
-        if order.is_valid():
+        order_form = PrintOrderForm(request.POST, request.FILES)
+        if order_form.is_valid() and validate_print_order_form(order_form):
             user = request.user
             new_print_order = PrintOrder()
 
             new_print_order.user = user
-            new_print_order.up_file = order.cleaned_data['file']
+            new_print_order.up_file = order_form.cleaned_data['file']
             new_print_order.time = datetime.datetime.now()
             new_print_order.status = OrderStatus().STATUS_UPLOADED
 
-            new_print_order.color = order.cleaned_data['color']
-            new_print_order.method = order.cleaned_data['method']
-            new_print_order.tel = escape(order.cleaned_data['tel'])
-
-            if validate.print_order_validate['filetype'](new_print_order.up_file.name) and \
-                    validate.print_order_validate['status'](str(new_print_order.status)) and validate.print_order_validate[
-                'method'](str(new_print_order.method)):
-                pass
-            else:
-                return show_success('upload fail', 'dashboard')
+            new_print_order.color = order_form.cleaned_data['color']
+            new_print_order.method = order_form.cleaned_data['method']
 
             new_print_order.save()
             return show_success('upload ok', 'dashboard')
-    else:
-        order = PrintOrderForm()
     return show_success('upload fail', 'dashboard')
 
 
-def trial_order(request):
-    file_obj = request.FILES.get('file', None)
-    if request.method == "POST":
-        uf = TrialOrderForm(request.POST, request.FILES)
-        if uf.is_valid():
-            new_trial_order = TrialOrder()
-            new_trial_order.name = uf.cleaned_data['name']
-            new_trial_order.phone = uf.cleaned_data['phone']
-            new_trial_order.building = uf.cleaned_data['building']
-            new_trial_order.file = uf.cleaned_data['file']
-            new_trial_order.status = OrderStatus().STATUS_UPLOADED
+def validate_trail_order_form(trial_form):
+    name = trial_form.cleaned_data['name']
+    phone = trial_form.cleaned_data['phone']
+    building = trial_form.cleaned_data['building']
+    file = trial_form.cleaned_data['file']
+    if validate.trial_order_validate['name'](name) and validate.trial_order_validate['phone'](phone) \
+            and validate.trial_order_validate['building'](building)\
+            and validate.trial_order_validate['filetype'](file.name):
+        return True
+    return False
 
-            if validate.trial_order_validate['name'](new_trial_order.name) and validate.trial_order_validate['phone'](
-                    new_trial_order.phone) and validate.trial_order_validate['building'](new_trial_order.building) and \
-                    validate.trial_order_validate['filetype'](new_trial_order.file.name) and \
-                    validate.trial_order_validate['status'](new_trial_order.status):
-                pass
-            else:
-                return show_success('upload fail', '/')
+
+def trial_order(request):
+    if request.method == "POST":
+        trial_form = TrialOrderForm(request.POST, request.FILES)
+        if trial_form.is_valid() and validate_trail_order_form(trial_form):
+            new_trial_order = TrialOrder()
+            new_trial_order.name = trial_form.cleaned_data['name']
+            new_trial_order.phone = trial_form.cleaned_data['phone']
+            new_trial_order.building = trial_form.cleaned_data['building']
+            new_trial_order.file = trial_form.cleaned_data['file']
+            new_trial_order.status = OrderStatus.STATUS_UPLOADED
+
             new_trial_order.save()
             return show_success('Upload ok', 'register')
-    else:
-        uf = TrialOrderForm()
-    # return render_to_response('upfile.html',{'uf':uf})
     return show_success('upload fail', '/')
-    # return HttpResponse('trail successfully')
 
 
 def up_file(request):
