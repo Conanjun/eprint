9c3f5cfc2021a3c6dddc41fe94fa5d7a243f3041
hackeris <hackeris@qq.com>
2015-05-14 12:30:24 +0800

    fix admin backend url conflect

--- backend/urls.py
+++ backend/urls.py
@@ -2,11 +2,11 @@ from django.conf.urls import patterns, include, url
 
 
 urlpatterns = patterns('',
-                       url(r'^$', 'backend.views.admin_index', name='admin_index_default'),
-                       url(r'^login', 'backend.views.admin_login', name='admin_login'),
-                       url(r'^index', 'backend.views.admin_index', name='admin_index'),
-                       url(r'^print_orders_list', 'backend.views.admin_print_orders', name='admin_print_orders'),
-                       url(r'^trial_orders_list', 'backend.views.admin_trial_orders', name='admin_trial_orders'),
+                       url(r'^$', 'backend.views.backend_index', name='backend_index_default'),
+                       url(r'^login', 'backend.views.backend_login', name='backend_login'),
+                       url(r'^index', 'backend.views.backend_index', name='backend_index'),
+                       url(r'^print_orders_list', 'backend.views.backend_print_orders', name='backend_print_orders'),
+                       url(r'^trial_orders_list', 'backend.views.backend_trial_orders', name='backend_trial_orders'),
                        url(r'^download_files/*', 'backend.views.download_files', name='download_files'),
                        )
 
--- backend/views.py
+++ backend/views.py
@@ -20,35 +20,35 @@ def staff_view(func):
     return _deco
 
 
-class AdminLoginForm(forms.Form):
+class BackendLoginForm(forms.Form):
     email = forms.EmailField(label='email', widget=forms.widgets.TextInput())
     password = forms.CharField(label='password', widget=forms.PasswordInput())
 
 
 def show_login_page(request):
     context = RequestContext(request)
-    return render_to_response('admin/login.html', context)
+    return render_to_response('backend/login.html', context)
 
 
-def admin_login(request):
+def backend_login(request):
     if request.method == 'POST':
-        return perform_admin_login(request)
+        return perform_backend_login(request)
     else:
         return show_login_page(request)
 
 
-def perform_admin_login(request):
-    admin_form = AdminLoginForm(request.POST)
-    if admin_form.is_valid():
-        email = admin_form.cleaned_data['email']
-        password = admin_form.cleaned_data['password']
+def perform_backend_login(request):
+    backend_form = BackendLoginForm(request.POST)
+    if backend_form.is_valid():
+        email = backend_form.cleaned_data['email']
+        password = backend_form.cleaned_data['password']
         try:
-            admin_user = User.objects.get(email=email)
+            backend_user = User.objects.get(email=email)
         except:
             return show_login_page(request)
-        admin_user_auth = authenticate(username=admin_user.username, password=password)
-        if admin_user_auth.is_staff:
-            login(request, admin_user_auth)
+        backend_user_auth = authenticate(username=backend_user.username, password=password)
+        if backend_user_auth.is_staff:
+            login(request, backend_user_auth)
             return HttpResponseRedirect('index')
         else:
             return show_login_page(request)
@@ -56,28 +56,28 @@ def perform_admin_login(request):
         return show_login_page(request)
 
 
-def admin_index(request):
+def backend_index(request):
     context = RequestContext(request)
-    return render_to_response('admin/index.html', context)
+    return render_to_response('backend/index.html', context)
 
 
 @staff_view
-def admin_print_orders(request):
+def backend_print_orders(request):
     context = RequestContext(request)
     print_orders_list = PrintOrder.objects.order_by("time")
     for orders in print_orders_list:
         print orders.user.email
     context['orders_list'] = print_orders_list
     # {"orders_list": print_orders_list}
-    return render_to_response('admin/print_orders.html', context)
+    return render_to_response('backend/print_orders.html', context)
 
 
 @staff_view
-def admin_trial_orders(request):
+def backend_trial_orders(request):
     context = RequestContext(request)
     trial_orders_list = TrialOrder.objects.order_by("name")
     context['trial_orders_list'] = trial_orders_list
-    return render_to_response('admin/trial_orders.html', context)
+    return render_to_response('backend/trial_orders.html', context)
 
 
 @staff_view
